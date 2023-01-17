ebe01acb67e880bea7e5bdb713fbf4f381ecd865
John Kidd Jr <johnjr@kiddclan.com>
2023-01-17 14:49:04 -0500

    setup window and project
    used imgui as gui for cross platform compatibility and performance when viewing and editing sprites

---
+++ CharacterDataEditor/Program.cs
@@ -0,0 +1,43 @@
+﻿using ImGuiNET;
+using Raylib_cs;
+
+namespace CharacterDataEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var logo = Raylib.LoadImage("Resources/logo.png");
+            //initial the graphics lib
+            Raylib.InitWindow(1920, 1080, "Knockout Arcade - Character Data Editor");
+            Raylib.SetWindowIcon(logo);
+            Raylib.SetTargetFPS(60);
+
+            //create a context to access ImGui
+            var context = ImGui.CreateContext();
+            ImGui.SetCurrentContext(context);
+
+            //create a reference to the gui controller
+            var controller = new GuiController();
+
+            //enter the program loop
+            while (!Raylib.WindowShouldClose())
+            {
+                controller.NewFrame();
+                controller.ProcessEvent();
+                ImGui.NewFrame();
+
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.DARKGRAY);
+
+                //render goes here
+
+                ImGui.Render();
+                controller.Render(ImGui.GetDrawData());
+                Raylib.EndDrawing();
+            }
+
+            controller.Shutdown();
+        }
+    }
+}
