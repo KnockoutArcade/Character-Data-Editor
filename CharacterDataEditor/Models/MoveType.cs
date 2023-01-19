c8b35faa8ebfeea32e8605c8be3449c85016fc2d
John Kidd Jr <johnjr@kiddclan.com>
2023-01-19 14:12:40 -0500

    added character data models for JSON exporting

---
+++ CharacterDataEditor/Models/MoveType.cs
@@ -0,0 +1,22 @@
+﻿namespace CharacterDataEditor.Models
+{
+    public enum MoveType
+    {
+        SideSpecial,
+        NeutralSpecial,
+        BackwardThrow,
+        ForwardThrow,
+        Grab,
+        JumpingHeavy,
+        JumpingMedium,
+        JumpingLight,
+        CrouchingHeavy,
+        CrouchingMedium,
+        CrouchingLight,
+        StandingHeavy,
+        StandingMedium,
+        StandingLight,
+        StandingLight2,
+        StandingLight3
+    }
+}
