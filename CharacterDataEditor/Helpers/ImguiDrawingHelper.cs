0375a7109a564bf4fecf0027a96eebc72b5333bc
John Kidd Jr <johnjr@kiddclan.com>
2023-01-24 23:34:28 -0500

    added a drawing helper to improve code reuse

---
+++ CharacterDataEditor/Helpers/ImguiDrawingHelper.cs
@@ -0,0 +1,45 @@
+﻿using CharacterDataEditor.Extensions;
+using ImGuiNET;
+
+namespace CharacterDataEditor.Helpers
+{
+    public static class ImguiDrawingHelper
+    {
+        public static void DrawIntInput(string label, ref int value)
+        {
+            ImGui.Columns(2);
+
+            //draw the input label
+            ImGui.Text(label.UpperCaseFirstLetter().AddSpacesToCamelCase());
+
+            ImGui.NextColumn();
+
+            //draw the input without a label
+            ImGui.InputInt($"##{label}", ref value);
+
+            ImGui.Columns(1);
+        }
+
+        public static void DrawBoolInput(string label, ref bool value)
+        {
+            ImGui.Columns(2);
+            ImGui.Text(label.UpperCaseFirstLetter().AddSpacesToCamelCase());
+
+            ImGui.NextColumn();
+            ImGui.Checkbox($"##{label}", ref value);
+
+            ImGui.Columns(1);
+        }
+
+        public static void DrawComboInput(string label, string[] comboItems, ref int value)
+        {
+            ImGui.Columns(2);
+            ImGui.Text(label.UpperCaseFirstLetter().AddSpacesToCamelCase());
+
+            ImGui.NextColumn();
+            ImGui.Combo($"##{label}", ref value, comboItems, comboItems.Length);
+
+            ImGui.Columns(1);
+        }
+    }
+}
