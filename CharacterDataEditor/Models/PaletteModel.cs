0e2bfe0340d7aebe488387b0354bdc4ba1c2d380
John Kidd Jr <johnjr@kiddclan.com>
2023-01-26 21:33:43 -0500

    added enumeration field

--- CharacterDataEditor/Models/PaletteModel.cs
+++ CharacterDataEditor/Models/PaletteModel.cs
@@ -5,6 +5,7 @@ namespace CharacterDataEditor.Models
     public record PaletteModel
     {
         public string Name { get; set; } = string.Empty;
+        public int NumberOfReplacableColors { get { return ColorPalette?.Count ?? 0; } }
         public List<RGBModel> ColorPalette { get; set; } = new List<RGBModel>();
     }
 }
