6e90d67380bf5e9e975f14595ec7e3b834eb372e
John Kidd Jr <johnjr@kiddclan.com>
2023-02-02 21:53:54 -0500

    added titles to new constants file

---
+++ CharacterDataEditor/Constants/TitleConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace CharacterDataEditor.Constants
+{
+    public static class TitleConstants
+    {
+        public const string Title = "Knockout Arcade - Character Data Editor";
+        public const string EditCharacterTitle = $"{Title} - Edit Character";
+        public const string ProjectHomeTitle = $"{Title} - Project Home";
+        public const string UnsavedIndicator = " ** Unsaved Work **";
+    }
+}
