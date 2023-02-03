f50c6aeec4ab69c61068eff12c1cfea1a13bb030
John Kidd Jr <johnjr@kiddclan.com>
2023-02-02 21:54:26 -0500

    added extension function to allow object copy

---
+++ CharacterDataEditor/Extensions/ObjectExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace CharacterDataEditor.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static T Clone<T>(this T obj)
+        {
+            return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(obj));
+        }
+    }
+}
