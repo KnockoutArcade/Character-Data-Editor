7a810d710fb55f34ba81795b913e083410949d90
John Kidd Jr <johnjr@kiddclan.com>
2023-02-07 13:55:32 -0500

    added required new data structures

---
+++ CharacterDataEditor/Models/OpponentPositionDataModel.cs
@@ -0,0 +1,44 @@
+﻿using CharacterDataEditor.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CharacterDataEditor.Models
+{
+    public class OpponentPositionDataModel
+    {
+        public int NumberOfFrames { get { return Frames?.Count ?? 0; } }
+        public float DistanceFromWall { get; set; } = 0.0f;
+        public List<OpponentPositionFrameModel> Frames { get; set; } = new List<OpponentPositionFrameModel>();
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(DistanceFromWall, Frames);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != typeof(OpponentPositionDataModel))
+            {
+                return false;
+            }
+
+            var objAsOpponentPositionData = obj as OpponentPositionDataModel;
+
+            if (objAsOpponentPositionData.DistanceFromWall == DistanceFromWall)
+            {
+                if (objAsOpponentPositionData.Frames.SequenceEqual(Frames))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ CharacterDataEditor/Models/OpponentPositionFrameModel.cs
@@ -0,0 +1,61 @@
+﻿using CharacterDataEditor.Enums;
+using System;
+
+namespace CharacterDataEditor.Models
+{
+    public class OpponentPositionFrameModel
+    {
+        public int Frame { get; set; } = 0;
+        public int RelativeX { get; set; } = 0;
+        public int RelativeY { get; set; } = 0;
+        public SpriteType Sprite { get; set; } = SpriteType.HurtSprite;
+        public int Index { get; set; } = 0;
+        public int Rotation { get; set; } = 0;
+        public int XScale { get; set; } = 0;
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Frame, RelativeX, RelativeY, Sprite, Index, Rotation, XScale);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != typeof(OpponentPositionFrameModel))
+            {
+                return false;
+            }
+
+            var objAsOpponentPositionFrame = obj as OpponentPositionFrameModel;
+
+            if (objAsOpponentPositionFrame.XScale == XScale)
+            {
+                if (objAsOpponentPositionFrame.Rotation == Rotation)
+                {
+                    if (objAsOpponentPositionFrame.Sprite == Sprite)
+                    {
+                        if (objAsOpponentPositionFrame.Frame == Frame)
+                        {
+                            if (objAsOpponentPositionFrame.Index == Index)
+                            {
+                                if (objAsOpponentPositionFrame.RelativeX == RelativeX)
+                                {
+                                    if (objAsOpponentPositionFrame.RelativeY == RelativeY)
+                                    {
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
