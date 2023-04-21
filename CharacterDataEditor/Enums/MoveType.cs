74fc586f8d3161af5716ff0a59fa9be5e8acb939
John Kidd Jr <johnjr@kiddclan.com>
2023-04-20 23:07:56 -0400

    updated move types to use flags, saved previous version for upgrades

--- CharacterDataEditor/Enums/MoveType.cs
+++ CharacterDataEditor/Enums/MoveType.cs
@@ -1,25 +1,28 @@
-﻿namespace CharacterDataEditor.Enums
+﻿using System;
+
+namespace CharacterDataEditor.Enums
 {
+    [Flags]
     public enum MoveType
     {
-        BackwardThrow = 0,
-        ForwardThrow = 1,
-        Grab = 2,
-        SideSpecial = 3,
-        NeutralSpecial = 4,
-        StandingLight = 5,
-        StandingLight2 = 6,
-        StandingLight3 = 7,
-        StandingMedium = 8,
-        StandingHeavy = 9,
-        CrouchingLight = 10,
-        CrouchingMedium = 11,
-        CrouchingHeavy = 12,
-        JumpingLight = 13,
-        JumpingMedium = 14,
-        JumpingHeavy = 15,
-        UpSpecial = 16,
-        DownSpecial = 17,
-        CommandGrab = 18
+        BackwardThrow = 0x00000,
+        ForwardThrow = 0x00001,
+        Grab = 0x00002,
+        SideSpecial = 0x00004,
+        NeutralSpecial = 0x00008,
+        StandingLight = 0x00010,
+        StandingLight2 = 0x00020,
+        StandingLight3 = 0x00040,
+        StandingMedium = 0x00080,
+        StandingHeavy = 0x00100,
+        CrouchingLight = 0x00200,
+        CrouchingMedium = 0x00400,
+        CrouchingHeavy = 0x00800,
+        JumpingLight = 0x01000,
+        JumpingMedium = 0x02000,
+        JumpingHeavy = 0x04000,
+        UpSpecial = 0x08000,
+        DownSpecial = 0x10000,
+        CommandGrab = 0x20000
     }
 }
