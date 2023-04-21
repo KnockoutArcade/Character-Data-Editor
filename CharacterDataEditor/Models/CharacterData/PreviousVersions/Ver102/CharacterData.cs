5f99bf4c0f4a8bf8db2f6abfdaf4a511ca5cc93f
John Kidd Jr <johnjr@kiddclan.com>
2023-04-20 23:08:46 -0400

    Saved changed files to new previous version folder

---
+++ CharacterDataEditor/Models/CharacterData/PreviousVersions/Ver102/CharacterData.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CharacterDataEditor.Models.CharacterData.PreviousVersions.Ver102
+{
+    public class CharacterDataModel : BaseCharacter
+    {
+        public string Name { get; set; } = string.Empty;
+        #region Base Stats
+        // movement
+        public float HorizontalSpeed { get; set; } = 0.0f;
+        public float VerticalSpeed { get; set; } = 0.0f;
+        public int EnvironmentalDisplacement { get; set; } = 0;
+        public float WalkSpeed { get; set; } = 0.0f;
+        public float RunSpeed { get; set; } = 0.0f;
+        public float Traction { get; set; } = 0.0f;
+        public float JumpSpeed { get; set; } = 0.0f;
+        public float FallSpeed { get; set; } = 0.0f;
+        // backdash
+        public int BackDashDuration { get; set; } = 0;
+        public int BackDashInvincibility { get; set; } = 0;
+        public float BackDashSpeed { get; set; } = 0.0f;
+        public float BackDashStartup { get; set; } = 0.0f;
+        // misc
+        public float FastFallSpeed { get; set; } = 0.0f;
+        public JumpType JumpType { get; set; } = JumpType.None;
+        public float JumpHorizontalSpeed { get; set; } = 0.0f;
+        public CharacterSpriteCollectionModel CharacterSprites { get; set; } = new CharacterSpriteCollectionModel();
+        public float SuperMeterBuildRate { get; set; } = 0.0f;
+        #endregion
+        public PaletteModel BaseColor { get; set; } = new PaletteModel();
+        public int NumberOfPalettes { get { return Palettes?.Count ?? 0; } }
+        public List<PaletteModel> Palettes { get; set; } = new List<PaletteModel>();
+        public List<MoveDataModel> MoveData { get; set; } = new List<MoveDataModel>();
+
+        public override int GetHashCode()
+        {
+            var hash = HashCode.Combine(Name, HorizontalSpeed, VerticalSpeed, EnvironmentalDisplacement, WalkSpeed, BaseColor, Palettes, MoveData);
+            hash = HashCode.Combine(hash, RunSpeed, Traction, JumpSpeed, FallSpeed, BackDashDuration, BackDashInvincibility, BackDashSpeed);
+            hash = HashCode.Combine(hash, BackDashStartup, FastFallSpeed, JumpType, JumpHorizontalSpeed, CharacterSprites, SuperMeterBuildRate);
+
+            return hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != typeof(CharacterDataModel))
+            {
+                return false;
+            }
+
+            var objAsCharacterDataModel = (CharacterDataModel)obj;
+
+            if (objAsCharacterDataModel.Name.Equals(Name))
+            {
+                if (objAsCharacterDataModel.BaseColor.Equals(BaseColor))
+                {
+                    if (objAsCharacterDataModel.Palettes.SequenceEqual(Palettes))
+                    {
+                        if (objAsCharacterDataModel.MoveData.SequenceEqual(MoveData))
+                        {
+                            if (objAsCharacterDataModel.HorizontalSpeed.Equals(HorizontalSpeed) &&
+                                objAsCharacterDataModel.VerticalSpeed.Equals(VerticalSpeed))
+                            {
+                                if (objAsCharacterDataModel.EnvironmentalDisplacement.Equals(EnvironmentalDisplacement))
+                                {
+                                    if (objAsCharacterDataModel.WalkSpeed.Equals(WalkSpeed) &&
+                                        objAsCharacterDataModel.RunSpeed.Equals(RunSpeed) &&
+                                        objAsCharacterDataModel.JumpSpeed.Equals(JumpSpeed) &&
+                                        objAsCharacterDataModel.FallSpeed.Equals(FallSpeed) &&
+                                        objAsCharacterDataModel.BackDashSpeed.Equals(BackDashSpeed) &&
+                                        objAsCharacterDataModel.FastFallSpeed.Equals(FastFallSpeed) &&
+                                        objAsCharacterDataModel.JumpHorizontalSpeed.Equals(JumpHorizontalSpeed))
+                                    {
+                                        if (objAsCharacterDataModel.Traction.Equals(Traction))
+                                        {
+                                            if (objAsCharacterDataModel.BackDashDuration.Equals(BackDashDuration) &&
+                                                objAsCharacterDataModel.BackDashInvincibility.Equals(BackDashInvincibility) &&
+                                                objAsCharacterDataModel.BackDashStartup.Equals(BackDashStartup) &&
+                                                objAsCharacterDataModel.JumpType.Equals(JumpType) &&
+                                                objAsCharacterDataModel.CharacterSprites.Equals(CharacterSprites) &&
+                                                objAsCharacterDataModel.SuperMeterBuildRate.Equals(SuperMeterBuildRate))
+                                            {
+                                                return true;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            return false;
+        }
+    }
+}
---
+++ CharacterDataEditor/Models/CharacterData/PreviousVersions/Ver102/MoveDataModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CharacterDataEditor.Enums.PreviousVersions;
+using Newtonsoft.Json;
+
+namespace CharacterDataEditor.Models.CharacterData.PreviousVersions.Ver102
+{
+    public class MoveDataModel
+    {
+        public string UID { get; set; } = Guid.NewGuid().ToString();
+        public MoveType MoveType { get; set; } = MoveType.BackwardThrow;
+        public string SpriteName { get; set; } = string.Empty;
+        public int Duration { get; set; } = 0;
+        public int NumberOfFrames { get { return FrameData?.Count ?? 0; } }
+        public List<FrameDataModel> FrameData { get; set; } = new List<FrameDataModel>();
+        public int NumberOfHitboxes { get { return AttackData?.Count ?? 0; } }
+        public List<AttackDataModel> AttackData { get; set; } = new List<AttackDataModel>();
+        public List<CounterHitDataModel> CounterData { get; set; } = new List<CounterHitDataModel>();
+        public bool IsThrow { get; set; } = false;
+        public OpponentPositionDataModel OpponentPositionData { get; set; } = new OpponentPositionDataModel();
+        public int NumberOfHurtboxes { get { return HurtboxData?.Count ?? 0; } }
+        public List<HurtboxDataModel> HurtboxData { get; set; } = new List<HurtboxDataModel>();
+        public int NumberOfProjectiles { get { return ProjectileData?.Count ?? 0; } }
+        public List<ProjectileDataModel> ProjectileData { get; set; } = new List<ProjectileDataModel>();
+        public RehitDataModel RehitData { get; set; } = new RehitDataModel();
+        public SupplimentaryMovementDataModel GroundMovementData { get; set; } = new SupplimentaryMovementDataModel();
+        public SupplimentaryMovementDataModel AirMovementData { get; set; } = new SupplimentaryMovementDataModel();
+        public string SupplimentaryScript { get; set; } = string.Empty;
+
+        public MoveDataModel GetDuplicate()
+        {
+            var tempSerializedMoveData = JsonConvert.SerializeObject(this);
+            var deserializedMoveData = JsonConvert.DeserializeObject<MoveDataModel>(tempSerializedMoveData);
+
+            deserializedMoveData.UID = Guid.NewGuid().ToString();
+            return deserializedMoveData;
+        }
+
+        public override int GetHashCode()
+        {
+            var hash = HashCode.Combine(UID, MoveType, SpriteName, FrameData, AttackData, IsThrow, HurtboxData, RehitData);
+            hash = HashCode.Combine(hash, OpponentPositionData, CounterData, GroundMovementData, AirMovementData, SupplimentaryScript, Duration, ProjectileData);
+
+            return hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != typeof(MoveDataModel))
+            {
+                return false;
+            }
+
+            var objAsMoveData = obj as MoveDataModel;
+
+            if (objAsMoveData.UID == UID)
+            {
+                if (objAsMoveData.MoveType == MoveType)
+                {
+                    if (objAsMoveData.SpriteName == SpriteName)
+                    {
+                        if (objAsMoveData.FrameData.SequenceEqual(FrameData))
+                        {
+                            if (objAsMoveData.AttackData.SequenceEqual(AttackData))
+                            {
+                                if (objAsMoveData.IsThrow == IsThrow)
+                                {
+                                    if (objAsMoveData.HurtboxData.SequenceEqual(HurtboxData))
+                                    {
+                                        if (objAsMoveData.OpponentPositionData.Equals(OpponentPositionData))
+                                        {
+                                            if (objAsMoveData.RehitData.Equals(RehitData))
+                                            {
+                                                if (objAsMoveData.GroundMovementData.Equals(GroundMovementData))
+                                                {
+                                                    if (objAsMoveData.AirMovementData.Equals(AirMovementData))
+                                                    {
+                                                        if (objAsMoveData.CounterData.SequenceEqual(CounterData))
+                                                        {
+                                                            if (objAsMoveData.Duration.Equals(Duration))
+                                                            {
+                                                                if (objAsMoveData.SupplimentaryScript.Equals(SupplimentaryScript))
+                                                                {
+                                                                    if (objAsMoveData.ProjectileData.SequenceEqual(ProjectileData))
+                                                                    {
+                                                                        return true;
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
