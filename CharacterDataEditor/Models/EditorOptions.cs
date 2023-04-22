ce223c84ed28a0be87d50bb59aa3fdf853a6139f
John Kidd Jr <johnjr@kiddclan.com>
2023-04-22 00:24:20 -0400

    Created editor options service, model, and added as singleton

---
+++ CharacterDataEditor/Models/EditorOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CharacterDataEditor.Models
+{
+    public class EditorOptionsModel
+    {
+        public string ThemeName { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
